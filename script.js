df68a3c7961feb79192261b9a057d14410f609f3
gleicekelly13 <gleice.kelly22@hotmail.com>
2022-08-19 01:08:15 -0300

    Alterando o JS

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 function insert(num)
 {
-    document.getElementById('resultado').innerHTML = num;
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+}
+
+function clean()
+{
+    document.getElementById('resultado').innerHTML = "";
+}
+
+function back()
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+
+function calcular()
+{
+    var resultado = document.getElementById("resultado").innerHTML;
+    if(resultado)
+    {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else
+    {
+        document.getElementById('resultado').innerHTML = "Nada..."
+    }
 }
